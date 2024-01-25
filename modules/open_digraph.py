59fc8d785c622e49563f0fcb6f8bd0d5ebae5812
Baptiste Pras <baptiste@macbook-air-de-baptiste.home>
2024-01-25 19:37:48 +0100

    All exercizes from Session 1 + tests on every method created

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -1,28 +1,125 @@
+from typing import List, Dict, Tuple
+
+
 class Node:
 
-    def __init__(self, identity, label, parents, children):
+    # Constructor
+    def __init__(self, identity: int, label: str, parents: Dict[int, int], children: Dict[int, int]) -> None:
         """
+        Construct a new node object
         identity: int; its unique id in the graph
         label: string;
         parents: int->int dict; maps a parent nodes id to its multiplicity
         children: int->int dict; maps a child nodes id to its multiplicity
+        (Not sure what the multiplicity is)
         """
         self.id = identity
         self.label = label
         self.parents = parents
         self.children = children
 
-    def __str__(self):
+    # Getters
+    def get_id(self) -> int:
+        """
+        Return node id
+        """
+        return self.id
+
+    def get_label(self) -> str:
+        """
+        Return node label
+        """
+        return self.label
+
+    def get_parents(self) -> Dict[int, int]:
+        """
+        Return node parents dict
+        """
+        return self.parents
+
+    def get_children(self) -> Dict[int, int]:
+        """
+        Return node children dict
+        """
+        return self.children
+
+    # Setters
+    def set_id(self, new_identity: int) -> None:
+        """
+        Change node id
+        new_identity: int; its unique id in the graph
+        """
+        self.id = new_identity
+
+    def set_label(self, new_label: str) -> None:
+        """
+        Change node label
+        new_label: string;
+        """
+        self.label = new_label
+
+    def set_children(self, new_children) -> None:
+        """
+        Change node children dict
+        new_children: int->int dict; maps a child nodes id to its multiplicity
+        """
+        self.children = new_children
+
+    def add_parent_id(self, parent_id) -> None:
+        """
+        Add a new parent to node parents dict
+        parent_id: int;
+        """
+        if parent_id in self.parents:  # Already a parent
+            self.parents[parent_id] += 1  # Increase the multiplicity
+        else:  # Not yet a parent
+            self.parents[parent_id] = 1
+
+    def add_child_id(self, child_id):
+        """
+        Add a new child to node children dict
+        child_id: int;
+        """
+        if child_id in self.children:  # Already a child
+            self.children[child_id] += 1  # Increase the multiplicity
+        else:  # Not yet a child
+            self.children[child_id] = 1
+
+    # Printing methods
+    def __str__(self) -> str:
+        """
+        Used by the __repr__ method
+        """
         return f'({self.id}, {self.label}, {self.parents}, {self.children})'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Used to print the object
+        """
         return str(self)
+
+    # Methods
+    def __eq__(self, other) -> bool:
+        """
+        Implementation of the "==" between two nodes
+        other: Node;
+        """
+        return (self.id == other.id and self.label == other.label and
+                self.parents == other.parents and self.children == other.children)
+
+    def copy(self):
+        """
+        Create a copy of the node
+        """
+        return Node(self.id, self.label, dict(self.parents), dict(self.children))
 
 
 class OpenDigraph:  # for open directed graph
 
-    def __init__(self, inputs, outputs, nodes):
+    # Constructors
+    def __init__(self, inputs: List[int], outputs: List[int], nodes: List[Node]) -> None:
         """
+        Construct a new OpenDigraph object
         inputs: int list; the ids of the input nodes
         outputs: int list; the ids of the output nodes
         nodes: node iter;
@@ -31,13 +128,164 @@ class OpenDigraph:  # for open directed graph
         self.outputs = outputs
         self.nodes = {node.id: node for node in nodes}  # self.nodes: <int,node> dict
 
-    def __str__(self):
-        return f'({self.inputs}, {self.outputs}, {self.nodes})'
-
-    def __repr__(self):
-        return str(self)
-
     @classmethod
     def empty(cls):
-        # Create an instance of the class with empty inputs, outputs, and nodes
+        """
+        Create an instance of the class with empty inputs, outputs, and nodes
+        """
         return cls(inputs=[], outputs=[], nodes=[])
+
+    # Getters
+    def get_input_ids(self) -> List[int]:
+        """
+        Return diagraph input list
+        """
+        return self.inputs
+
+    def get_output_ids(self) -> List[int]:
+        """
+        Return diagraph output list
+        """
+        return self.outputs
+
+    def get_id_node_map(self) -> Dict[int, Node]:
+        """
+        Return diagraph nodes dictionnary
+        """
+        return self.nodes
+
+    def get_nodes(self) -> List[Node]:
+        """
+        Return a list of all the nodes
+        """
+        return list(self.nodes.values())
+
+    def get_node_ids(self) -> List[int]:
+        """
+        Return a list of all the ids
+        """
+        return list(self.nodes.keys())
+
+    def get_node_by_id(self, node_id: int) -> Node:
+        """
+        Return a node given its id
+        """
+        return self.nodes[node_id]
+
+    def get_nodes_by_ids(self, ids: List[int]) -> List[Node]:
+        """
+        Return a list of nodes given a list of ids
+        """
+        return [self.nodes[node_id] for node_id in ids if node_id in self.nodes]
+
+    # Setters
+    def set_inputs(self, new_inputs: List[int]) -> None:
+        """
+        Change diagraph input list
+        new_inputs: List[int];
+        """
+        self.inputs = new_inputs
+
+    def set_outputs(self, new_outputs: List[int]) -> None:
+        """
+        Change diagraph output list
+        new_outputs: List[int];
+        """
+        self.outputs = new_outputs
+
+    def add_input_id(self, input_id: int) -> None:
+        """
+        Add a new input to the input list
+        input_id: int;
+        """
+        if input_id not in self.inputs:  # useless to add it twice
+            self.inputs.append(input_id)
+
+    def add_output_id(self, output_id: int) -> None:
+        """
+        Add a new input to the input list
+        input_id: int;
+        """
+        if output_id not in self.outputs:  # useless to add it twice
+            self.outputs.append(output_id)
+
+    # Printing methods
+    def __str__(self) -> str:
+        """
+        Used by the __repr__ method
+        """
+        return f'({self.inputs}, {self.outputs}, {self.nodes})'
+
+    def __repr__(self) -> str:
+        """
+        Used to print the object
+        """
+        return str(self)
+
+    # Methods
+    def __eq__(self, other) -> bool:
+        """
+        Implementation of the "==" between two diagraphs
+        other: OpenDiagraph;
+        """
+        return (self.inputs == other.inputs and self.outputs == other.outputs
+                and self.nodes == other.nodes)
+
+    def copy(self):
+        """
+        Create a copy of the graph
+        """
+        return OpenDigraph(list(self.inputs), list(self.outputs), list(self.nodes.values()))
+
+    def new_id(self):
+        """
+        Find and return an unused ID in the graph
+        """
+        used_ids = set(self.inputs + self.outputs + list(self.nodes.keys()))
+        return max(used_ids) + 1 if used_ids else 0  # If there's no existing id, return 0, else the max + 1
+        # We suppose that if we have n ids, they all go from 0 to n-1
+        # Still doesn't cause any problem if we don't have that condition
+        # We'll just have ids not numerotated from 0 to n-1
+        # But this function is faster (O(n)) than checking for the first unused id from 0 to n (O(n^2))
+
+    def add_edge(self, src: int, tgt: int) -> None:
+        """
+        Add an edge from the source node to the target node
+        src: int; source node
+        tgt: int; target node
+        """
+        if src in self.nodes and tgt in self.nodes:
+            self.get_node_by_id(src).add_child_id(tgt)  # Add a child to the source node
+            self.get_node_by_id(tgt).add_parent_id(src)  # Add a parent to the target node
+
+    def add_edges(self, edges: List[Tuple[int, int]]) -> None:
+        """
+        Add edges between each pair of node IDs in the list of edges
+        edges: list(tuple(int, int)); list of edges to add (int; source node, int; target node)
+        """
+        for src, tgt in edges:
+            self.add_edge(src, tgt)
+
+    def add_node(self, label="", parents=None, children=None) -> int:
+        """
+        Adds a node with a label to the graph, assigning it a new id.
+        Links it with the parent and child id nodes with their respective multiplicities.
+        If the default values for parents and/or children are None, assign them an empty dictionary.
+        Returns the id of the new node.
+        label: string;
+        parents: List[int]; ids of the parents
+        children: List[int]; ids of the childrens
+        """
+        # Create an new object Node
+        new_node = Node(self.new_id(), label, {}, {})
+        if parents is not None:
+            for parent in parents:
+                new_node.add_parent_id(parent)
+        if children is not None:
+            for child in children:
+                new_node.add_child_id(child)
+
+        # Add the new node to the graph
+        self.nodes[new_node.get_id()] = new_node
+
+        return new_node.get_id()
--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -29,6 +29,106 @@ class InitTest(unittest.TestCase):
         self.assertEqual(g.outputs, [])
         self.assertEqual(len(g.nodes), 0)
 
+    def test_copy_Node(self):
+        n = Node(0, 'i', {}, {1: 1})
+        self.assertIsNot(n.copy(), n)
+
+    def test_copy_OpenDigraph(self):
+        g = OpenDigraph([0], [0], [Node(0, 'i', {}, {1: 1})])
+        self.assertIsNot(g.copy(), g)
+
+    def test_getters_Node(self):
+        n = Node(0, 'i', {}, {1: 1})
+        self.assertEqual(n.get_id(), n.id)
+        self.assertEqual(n.get_label(), n.label)
+        self.assertEqual(n.get_parents(), n.parents)
+        self.assertEqual(n.get_children(), n.children)
+
+    def test_getters_OpenDigraph(self):
+        n0 = Node(0, 'Orsay', {}, {2: 1})
+        n1 = Node(1, 'Le Guichet', {}, {2: 1})
+        n2 = Node(2, 'Paris', {0: 1, 1: 1}, {})
+        g = OpenDigraph([0, 1], [2], [n0, n1, n2])
+        self.assertEqual(g.get_input_ids(), g.inputs)
+        self.assertEqual(g.get_output_ids(), g.outputs)
+        self.assertEqual(g.get_id_node_map(), g.nodes)
+        self.assertEqual(g.get_nodes(), [n0, n1, n2])
+        self.assertEqual(g.get_node_ids(), [0, 1, 2])
+        self.assertEqual(g.get_node_by_id(1), n1)
+        self.assertEqual(g.get_nodes_by_ids([0, 2]), [n0, n2])
+
+    def test_setters_Node(self):
+        n = Node(0, 'i', {}, {1: 1})
+        n.set_id(1)
+        n.set_label('j')
+        n.set_children({2: 1, 0: 1})
+        n.add_parent_id(2)
+        n.add_parent_id(2)
+        n.add_child_id(1)
+        n.add_child_id(0)
+        self.assertEqual(n.id, 1)
+        self.assertEqual(n.label, 'j')
+        self.assertEqual(n.parents, {2: 2})
+        self.assertEqual(n.children, {2: 1, 0: 2, 1: 1})
+
+    def test_setters_OpenDigraph(self):
+        g = OpenDigraph.empty()
+        g.set_inputs([1, 2])
+        g.set_outputs([3, 4])
+        g.add_input_id(3)
+        g.add_input_id(3)
+        g.add_output_id(2)
+        g.add_output_id(2)
+        self.assertEqual(g.inputs, [1, 2, 3])
+        self.assertEqual(g.outputs, [3, 4, 2])
+
+    def test_eq_Node(self):
+        n0 = Node(0, 'Orsay', {}, {2: 1})
+        n1 = Node(1, 'Le Guichet', {}, {2: 1})
+        self.assertEqual(n0, n0)
+        self.assertNotEqual(n0, n1)
+
+    def test_eq_OpenDiagraph(self):
+        n0 = Node(0, 'Orsay', {}, {2: 1})
+        n1 = Node(1, 'Le Guichet', {}, {2: 1})
+        n2 = Node(0, 'Orsay', {}, {2: 1})
+        g1 = OpenDigraph.empty()
+        g2 = OpenDigraph([3, 4], [6], [n0, n1, n2])
+        g3 = OpenDigraph([3, 4], [], [n0, n1, n2])
+        g4 = OpenDigraph([3, 4], [6], [n2])
+        self.assertEqual(g1, g1)
+        self.assertEqual(g2, g2)
+        self.assertNotEqual(g2, g3)
+        self.assertNotEqual(g2, g4)
+
+    def test_new_id_OpenDigraph(self):
+        g1 = OpenDigraph.empty()
+        n0 = Node(0, 'Orsay', {}, {2: 1})
+        n1 = Node(1, 'Le Guichet', {}, {2: 1})
+        n2 = Node(2, 'Paris', {0: 1, 1: 1}, {})
+        g2 = OpenDigraph([3, 4], [6], [n0, n1, n2])
+        self.assertEqual(g1.new_id(), 0)
+        self.assertEqual(g2.new_id(), 7)
+
+    def test_add_edges_OpenDigraph(self):
+        # Test add_edge in the same time
+        n0 = Node(0, 'Orsay', {}, {})
+        n1 = Node(1, 'Le Guichet', {}, {})
+        n2 = Node(2, 'Paris', {}, {})
+        g = OpenDigraph([3, 4], [6], [n0, n1, n2])
+        g.add_edges([(0, 1), (1, 2)])
+        self.assertEqual(g.get_node_by_id(0), Node(0, 'Orsay', {}, {1: 1}))
+        self.assertEqual(g.get_node_by_id(1), Node(1, 'Le Guichet', {0: 1}, {2: 1}))
+        self.assertEqual(g.get_node_by_id(2), Node(2, 'Paris', {1: 1}, {}))
+
+    def test_add_node_OpenDigraph(self):
+        n0 = Node(0, 'Orsay', {}, {})
+        n1 = Node(1, 'Le Guichet', {}, {})
+        n2 = Node(7, 'Paris', {1: 1}, {})
+        g = OpenDigraph([3, 4], [6], [n0, n1])
+        g.add_node('Paris', [1])
+        self.assertEqual(g, OpenDigraph([3, 4], [6], [n0, n1, n2]))
+
 
 if __name__ == '__main__':  # the following code is called only when
     unittest.main()  # precisely this file is run
